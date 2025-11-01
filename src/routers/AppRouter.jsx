a8244664310b8ca709124912fd659866a18b8f02
Utsho MH <mahadyhassanutsho@gmail.com>
2025-11-01 18:11:03 +0600

    added firebase and auth-provider

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 import AppLayout from "../layouts/AppLayout";
+import HomePage from "../pages/HomePage";
 
-const router = createBrowserRouter([{ path: "/", Component: AppLayout }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: AppLayout,
+    children: [{ path: "", index: true, Component: HomePage }],
+  },
+]);
 
 const AppRouter = ({ children }) => {
   return <RouterProvider router={router}>{children}</RouterProvider>;
